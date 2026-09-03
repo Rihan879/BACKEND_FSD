7ff60790e071016ae0e2fdb9ee4b5b6d20eb25cf
Rihan879 <rihansaifi0687@gmail.com>
2026-09-03 11:31:58 +0530

    GET and POST

--- REST/index.js
+++ REST/index.js
@@ -1,23 +1,26 @@
 import express from 'express';
-const app = express();
+const app=express();
 app.use(express.json());
 let users=[
-    {id: 1 , name:"Rihan", email: "abc@gmail.com"}
+    {id:1,name:"Rihan",email:"rihan@gmail.com"},
+    {id:2,name:"saifi",email:"saifi@gmail.com"}
 ];
 
-app.get('/user',(req,res)=>{
+//Get:get request to fetch all users
+app.get('/users',(req,res)=>{
     res.json(users);
 })
 
-app.post('/user',(req,res)=>{
-const user={
-    id:users.length+1,
-    name:req.body.name,
-    email:req.body.email
+//Post:post  request to crate a new user
+app.post('/users',(req,res)=>{
+    const user={
+        id:users.length+1,
+        name:req.body.name,
+        email:req.body.email
 };
 users.push(user);
-res.json(user);
+res.json(user);  
 })
 app.listen(8000,()=>{
-    console.log('server is running on port 8000');
-})
+    console.log("server is running on port: https://localhost:8000 ")
+});
