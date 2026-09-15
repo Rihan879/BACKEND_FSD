e8627ed258d3040f2009acfd7ffdaa08da941340
Rihan879 <rihansaifi0687@gmail.com>
2026-09-15 09:42:25 +0530

    assignment 3

---
+++ ASSIGNMENTS/portfolio/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+    console.log(`Portfolio server running on http://localhost:${PORT}`);
+});
